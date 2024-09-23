1daa749ffc91a45ea6c55e51729f41dd59941646
ShadowjGames <98559091+ShadowjGames@users.noreply.github.com>
2024-09-23 08:46:49 -0500

    Lightning Touch

--- Assets/Clase/Iluminación/Includes/Lightning.hlsl
+++ Assets/Clase/Iluminación/Includes/Lightning.hlsl
@@ -2,13 +2,13 @@
 //#Include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
 void GetMainLight_float(out float3 direction, out float3 color){
-	#if !defined(SHADERGRAPH_PREVIEW)
-	Light light;
-	Light = GetMainLight();
-	direction = Light.direction;
-	color = Light.color;
-	#else
-	direction = float3(1, 1, -1);
-	color = 1;
-	#endif
+#if !defined(SHADERGRAPH_PREVIEW)
+    Light light;
+    light = GetMainLight();
+    direction = light.direction;
+    color = light.color;
+#else
+    direction = float3(1, 1, -1);
+    color = 1;
+#endif
 }
